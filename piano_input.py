721304d426a5173bec1e4720e3663d19b451283d
zvika <zvika@google.com>
2015-12-27 14:02:49 +0200

    Add README and Apache license information

--- piano_input.py
+++ piano_input.py
@@ -1,9 +1,25 @@
+"""Class for handling input from a piano connected through a USB MIDI.
+
+Copyright 2015 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import Queue
 import thread
 import usb.core
 
 class PianoInput(object):
-  """Class for handling input from a piano connected through a USB MIDI."""
   def __init__(self):
     self.user_input = Queue.Queue()
     self.dev = usb.core.find(idVendor=0xfc08, idProduct=0x0101)
--- piano_input_mock.py
+++ piano_input_mock.py
@@ -1,8 +1,24 @@
+"""Mock version of piano_input.PianoInput.
+For use when testing on a computer without an actual MIDI input.
+
+Copyright 2015 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import Queue
 
 class PianoInput(object):
-  """Mock version of piano_input.PianoInput, for use when testing on a computer
-  without an actual MIDI input."""
   def __init__(self):
     self.user_input = Queue.Queue()
     for note in xrange(37,60):
